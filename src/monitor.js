b861c7a9ae43b1767e433599b2464c47c6aeaf7a
hotoo <hotoo.cn@gmail.com>
2013-05-08 14:36:58 +0800

    U: 补充注释等

--- src/monitor.js
+++ src/monitor.js
@@ -164,7 +164,7 @@ define(function(require, exports, module) {
     ref: doc.referrer || "-",
     //sys: servName,
     // XXX: 使用 detector v1.1.0 使用 full_version 属性。
-    client: detector.device.name+"/"+String(detector.device.version)+"|"+
+    clnt: detector.device.name+"/"+String(detector.device.version)+"|"+
       detector.os.name+"/"+String(detector.os.version)+"|"+
       detector.browser.name+"/"+String(detector.browser.version)+"|"+
       detector.engine.name+"/"+String(detector.engine.version),
--- src/seer.js
+++ src/seer.js
@@ -1,50 +1,70 @@
 (function() {
   if(window.monitor){return;}
 
-  var M = {};
+  var M = window.monitor = {};
   M._DATAS = [];
 
-  // XXX:
-  M.log = function(g, p, c) {
-    M._DATAS.push({
-      constom: {
-        type: "log",
-        productLine: g,
-        product: p,
-        errorCode: c,
-        counts: 1
-      }
-    });
-    if ("function" == typeof M.timedSend) {
-      M.timedSend();
+  /**
+   * 通用监控接口。
+   * @param {String} seed, 监控点。
+   * @param {String} profile, 监控类型，默认为 `log`。
+   * @return {Object}
+   */
+  M.log = function(seed, profile){
+    if(!seed){return;}
+    var profile = profile || "log";
+
+    // 兼容老版对产品监控的支持。
+    if(arguments.length === 3){
+      profile = "product";
+      seed = Array.prototype.join.call(arguments,"|");
     }
+    var data = {
+      profile: profile,
+      seed: String(seed)
+    };
+    M._DATAS.push(data);
+    return data;
   };
 
+  // TODO: `time` 统计数值计算的接口。
+
+  /**
+   * JavaScript 异常统一处理函数。
+   * @param {String} message, 异常消息。
+   * @param {String} file, 异常所在文件。
+   * @param {Number} line, 异常所在行。
+   * @return {Object} 主要用于单元测试，本身可以不返回。
+   */
+  function error(message, file, line){
+    var data = {
+      profile: "jserror",
+      msg: message || "",
+      file: file || "",
+      line: line || 0
+    };
+    M._DATAS.push(data);
+    return data;
+  }
+
+  /**
+   * JavaScript 异常接口，用于监控 `try/catch` 中被捕获的异常。
+   * @param {Error} err, JavaScript 异常对象。
+   * @return {Object} 主要用于单元测试。
+   */
   M.error = function(e) {
     if (!(e instanceof Error)) {return;}
-    M._DATAS.push({
-      profile: "jserror",
-      msg: e.message,
-      file: e.fileName || "",
-      ln: e.lineNumber || e.line || 0
-    });
-    if ("function" == typeof M.timedSend) {
-      M.timedSend();
-    }
+    return error(e.message, e.fileName, e.lineNumber || e.line);
   };
 
   /**
-   * JavaScript 异常监控。
+   * 全局 JavaScript 异常监控。
+   * @return {Boolean} 返回 `true` 则控制台捕获异常。
+   *                   返回 `false` 则控制台不捕获异常。
+   *                   建议返回 `false`。
    */
-  window.onerror = function(m, f, l) {
-    M._DATAS.push({
-      profile: "jserror",
-      msg: m,
-      file: f,
-      line: l
-    });
+  window.onerror = function(message, file, line) {
+    error(message, file, line);
     return false;
   }
-
-  window.monitor = M;
 })();
