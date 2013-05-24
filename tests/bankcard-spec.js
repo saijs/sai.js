3396ec1768518b135ee95dc12d471c3acd9ac95c
hotoo <hotoo.cn@gmail.com>
2013-05-24 13:44:24 +0800

    U: 新增几个测试银行卡号

--- tests/bankcard-spec.js
+++ tests/bankcard-spec.js
@@ -12,6 +12,11 @@ define(function(require) {
       expect(BankCard.verify("6226095711688726")).to.equal(true);
       expect(BankCard.verify("6225885860600709")).to.equal(true);
 
+      // 杭州银行
+      expect(BankCard.verify("603367100131942126")).to.equal(true);
+      // 广发
+      expect(BankCard.verify("6225683428000243950")).to.equal(true);
+
       expect(BankCard.verify("5187100010223842")).to.equal(true);
       expect(BankCard.verify("603367100131942126")).to.equal(true);
       expect(BankCard.verify("6225683428000243950")).to.equal(true);
