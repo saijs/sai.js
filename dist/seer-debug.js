290bccdae1f495593bddb93d5ecf9245856db3fc
hotoo <hotoo.cn@gmail.com>
2013-10-29 19:58:01 +0800

    A: build dist files

--- dist/seer-debug.js
+++ dist/seer-debug.js
@@ -1,19 +1,11 @@
-(function(global) {
+
+(function(global){
+
   if(global.monitor){return;}
 
   var M = global.monitor = {};
   M._DATAS = [];
 
-  var lost_resources = [];
-  var _lost_resources = {};
-  /**
-   * 客户端资源加载失败时调用这个接口。
-   */
-  M.lost = function(uri){
-    if(_lost_resources.hasOwnProperty(uri)){return;}
-    _lost_resources[uri] = true;
-    lost_resources.push(uri);
-  };
 
   var EVENTS = M._EVENTS = [];
   M.on = function(evt, handler){
@@ -21,17 +13,20 @@
   };
   M.off = function(){};
 
+
   var DEFAULT_PROFILE = "log";
-  /**
-   * 通用监控接口。
-   * @param {String} seed, 监控点。
-   * @param {String} profile, 监控类型，默认为 `log`。
-   * @return {Object}
-   */
+
+  // 通用监控接口。
+  // @param {Object,String} seed, 监控信息详情。
+  // @param {String} profile, 监控类型，默认为 `log`。
+  // @return {Object}
   M.log = function(seed, profile){
+
     if(!seed){return;}
-    // 老版产品监控。
+
+    // 取消老版产品监控。
     if(arguments.length >= 3){return;}
+
     var data;
     if(Object.prototype.toString.call(seed) === "[object Object]"){
       data = seed;
@@ -46,27 +41,41 @@
     return data;
   };
 
-  var RE_FUNCTION = /^function\b[^\)]+\)/;
-  /**
-   * 获得函数名。
-   * @param {Function} func, 函数对象。
-   * @return {String} 函数名。
-   */
+})(this);
+
+(function(global, monitor){
+
+  if(!monitor){return;}
+
+  var DEFAULT_PROFILE = "jserror";
+  var MAX_STACKTRACE_DEEP = 20;
+  var RE_FUNCTION = /^\s*function\b[^\)]+\)/;
+
+  var lost_resources = [];
+  var lost_resources_cache = {};
+
+  // 客户端资源加载失败时调用这个接口。
+  monitor.lost = function(uri){
+    if(lost_resources_cache.hasOwnProperty(uri)){return;}
+    lost_resources_cache[uri] = true;
+
+    lost_resources.push(uri);
+  };
+
+  // 获得函数名。
+  // @param {Function} func, 函数对象。
+  // @return {String} 函数名。
   function function_name(func){
     var match = String(func).match(RE_FUNCTION);
-    return match ? match[0] : "";
+    return match ? match[0] : "global";
   }
 
-  /**
-   * 函数调用堆栈。
-   * XXX: 匿名函数的支持。
-   * @param {Function} call, function's caller.
-   * @return {String} stack trace.
-   */
+  // 函数调用堆栈。
+  // @param {Function} call, function's caller.
+  // @return {String} stack trace.
   function stacktrace(call){
     var stack = [];
     var deep = 0;
-    var max_deep = 20;
 
     while(call.arguments && call.arguments.callee && call.arguments.callee.caller){
 
@@ -78,68 +87,71 @@
       // of the stack. The following line works around this.
       if (call.caller === call){break;}
 
-      if((deep++) > max_deep){break;}
+      if((deep++) > MAX_STACKTRACE_DEEP){break;}
     }
     return stack.join("\n");
   }
 
+  // 用于缓存识别同一个异常。
   var ERROR_CACHE = {};
-  /**
-   * JavaScript 异常统一处理函数。
-   * @param {String} message, 异常消息。
-   * @param {String} file, 异常所在文件。
-   * @param {Number} line, 异常所在行。
-   * @param {Number,String} number, 异常编码，IE 支持。
-   * @return {Object} 主要用于单元测试，本身可以不返回。
-   */
-  function error(message, file, line, number, stack){
+
+  // JavaScript 异常统一处理函数。
+  // @param {String} catchType, 捕获异常的类型。
+  // @param {String} message, 异常消息。
+  // @param {String} file, 异常所在文件。
+  // @param {Number} line, 异常所在行。
+  // @param {Number,String} number, 异常编码，IE 支持。
+  // @return {Object} 主要用于单元测试，本身可以不返回。
+  function error(catchType, message, file, line, number, stack){
     if(!stack && arguments.callee.caller){
       stack = stacktrace(arguments.callee.caller);
     }
+
     var data = {
-      profile: "jserror",
+      profile: DEFAULT_PROFILE,
+      type: catchType,
       msg: message || "",
       file: file || "",
       line: line || 0,
       num: number || "",
       stack: stack || "",
+      lang: navigator.language || navigator.browserLanguage || "",
       lost: lost_resources.join(",")
     };
-    var key = file+":"+line+":"+message;
+
+    var key = file + ":" + line + ":" + message;
     if(!ERROR_CACHE.hasOwnProperty(key)){
       data.uv = 1;
       ERROR_CACHE[key] = true;
     }
-    M._DATAS.push(data);
-    return data;
+
+    return monitor.log(data);
   }
 
-  /**
-   * JavaScript 异常接口，用于监控 `try/catch` 中被捕获的异常。
-   * @param {Error} err, JavaScript 异常对象。
-   * @return {Object} 主要用于单元测试。
-   */
-  M.error = function(ex){
-    if (!(ex instanceof Error)) {return;}
-    var stack = ex.stack || ex.stacktrace;
+
+  // JavaScript 异常接口，用于监控 `try/catch` 中被捕获的异常。
+  // @param {Error} err, JavaScript 异常对象。
+  // @return {Object} 主要用于单元测试。
+  monitor.error = function(ex){
+    if(!(ex instanceof Error)){return;}
+
     return error(
+      "catched",
       ex.message || ex.description,
       ex.fileName,
       ex.lineNumber || ex.line,
       ex.number,
-      stack
+      ex.stack || ex.stacktrace
     );
   };
 
-  /**
-   * 全局 JavaScript 异常监控。
-   * @return {Boolean} 返回 `true` 则控制台捕获异常。
-   *                   返回 `false` 则控制台不捕获异常。
-   *                   建议返回 `false`。
-   */
+  // 全局 JavaScript 异常监控。
+  // @return {Boolean} 返回 `true` 则捕获异常，浏览器控制台不显示异常信息。
+  //                   返回 `false` 则不捕获异常，浏览器控制台显示异常信息。
+  //                   建议返回 `false`。
   global.onerror = function(message, file, line) {
-    error(message, file, line);
+    error("global", message, file, line);
     return false;
   };
 
-})(this);
+})(this, this.monitor);
