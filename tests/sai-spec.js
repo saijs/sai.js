9ecbab2ac331522c7da95b6b6df5d8abcd820d7b
hotoo <hotoo.cn@gmail.com>
2014-07-31 15:19:52 +0800

    Update test cases by Imageic.

--- tests/sai-spec.js
+++ tests/sai-spec.js
@@ -2,49 +2,69 @@
 require("../seer-sai");
 
 var Image = require("imagic");
+var Url = require("url");
 var expect = require("expect.js");
-var util = require("./unitutil");
+var Sai;
 
 describe("Sai", function() {
 
     it("Sai.log(seed)", function(done) {
 
-      require.async("../sai", function(Sai){
-        window.Sai = Sai;
+      require.async("../sai", function(sai){
+        Sai = sai;
         Sai.server = "http://log.example.com/sai.gif"
 
-        expect(util.equals(Sai.log("seed"), {
-          profile: "log",
-          seed: "seed"
-        })).to.equal(true);
+        Image.on("fetch", function(src){
+          var url = new Url(src);
+          expect(url.getParam("seed")).to.equal("seed");
+          expect(url.getParam("profile")).to.equal("log");
 
-        done();
+          Image.off("fetch");
+          done();
+        });
+
+        Sai.log("seed");
       });
     });
 
     it("Sai.log(seed, profile)", function(done) {
-      expect(util.equals(Sai.log("seed", "profile-0"), {
-        profile: "profile-0",
-        seed: "seed"
-      })).to.equal(true);
-      done();
+      Image.on("fetch", function(src){
+        var url = new Url(src);
+        expect(url.getParam("seed")).to.equal("seed-0");
+        expect(url.getParam("profile")).to.equal("profile-0");
+
+        Image.off("fetch");
+        done();
+      });
+
+      Sai.log("seed-0", "profile-0");
     });
 
     it("Sai.log(object)", function(done) {
-      expect(util.equals(Sai.log({a:1, b:true}), {
-        profile: "log",
-        a: 1,
-        b: true
-      })).to.equal(true);
-      done();
+      Image.on("fetch", function(src){
+        var url = new Url(src);
+        expect(url.getParam("a")).to.equal("1");
+        expect(url.getParam("b")).to.equal("true");
+        expect(url.getParam("profile")).to.equal("log");
+
+        Image.off("fetch");
+        done();
+      });
+
+      Sai.log({a:1, b:true});
     });
 
-    it("Sai.log(object, profile)", function() {
-      expect(util.equals(Sai.log({a:1, b:true}, "profile-1"), {
-        profile: "profile-1",
-        a: 1,
-        b: true
-      })).to.equal(true);
+    it("Sai.log(object, profile)", function(done) {
+      Image.on("fetch", function(src){
+        var url = new Url(src);
+        expect(url.getParam("a")).to.equal("2");
+        expect(url.getParam("b")).to.equal("false");
+        expect(url.getParam("profile")).to.equal("profile-1");
+
+        Image.off("fetch");
+        done();
+      });
+      Sai.log({a:2, b:false}, "profile-1");
     });
 
     after(test_monitor_error)
@@ -52,126 +72,169 @@ describe("Sai", function() {
 });
 
 
-  function test_monitor_error(){
+function test_monitor_error(){
 
-    require("../seer-jsniffer");
+  require("../seer-jsniffer");
 
-    describe("Sai: jsniffer", function() {
+  describe("Sai: jsniffer", function() {
 
-      it("Sai.error()", function(done) {
-        var ex = Sai.error(new Error("error message."));
+    it("Sai.error()", function(done) {
+      Image.on("fetch", function(src){
+        var url = new Url(src);
+        expect(url.getParam("msg")).to.equal("error message.");
+        expect(url.getParam("profile")).to.equal("jserror");
+        expect(url.getParam("file")).not.to.be(null);
+        expect(url.getParam("line")).not.to.be(null);
+        expect(url.getParam("col")).not.to.be(null);
+        expect(url.getParam("num")).not.to.be(null);
+        expect(url.getParam("type")).not.to.be(null);
+        expect(url.getParam("stack")).not.to.be(null);
+        expect(url.getParam("lost")).not.to.be(null);
+        expect(url.getParam("lang")).not.to.be(null);
+        expect(url.getParam("uv")).to.equal("1");
 
-        expect(ex.msg).to.equal("error message.");
-        expect(ex.profile).to.equal("jserror");
-        expect(ex.hasOwnProperty("file")).to.equal(true);
-        expect(ex.hasOwnProperty("line")).to.equal(true);
-        expect(ex.hasOwnProperty("col")).to.equal(true);
-        expect(ex.hasOwnProperty("num")).to.equal(true);
-        expect(ex.hasOwnProperty("type")).to.equal(true);
-        expect(ex.hasOwnProperty("stack")).to.equal(true);
-        expect(ex.hasOwnProperty("lost")).to.equal(true);
-        expect(ex.hasOwnProperty("lang")).to.equal(true);
-        expect(ex.hasOwnProperty("uv")).to.equal(true);
-        expect(ex.uv).to.equal(1);
+        Image.off("fetch");
+
         done();
-      });
+      })
 
-      it("Sai.error() repeat, without uv.", function(done) {
-        // 异常消息必须已抛出过。
-        var ex1 = Sai.error(new Error("error message."));
-
-        expect(ex1.msg).to.equal("error message.");
-        expect(ex1.profile).to.equal("jserror");
-        expect(ex1.hasOwnProperty("file")).to.equal(true);
-        expect(ex1.hasOwnProperty("line")).to.equal(true);
-        expect(ex1.hasOwnProperty("col")).to.equal(true);
-        expect(ex1.hasOwnProperty("num")).to.equal(true);
-        expect(ex1.hasOwnProperty("type")).to.equal(true);
-        expect(ex1.hasOwnProperty("stack")).to.equal(true);
-        expect(ex1.hasOwnProperty("lost")).to.equal(true);
-        expect(ex1.hasOwnProperty("lang")).to.equal(true);
-        expect(ex1.hasOwnProperty("uv")).to.equal(false);
-        done();
-      });
-
-      it("try/catch: Sai.error()", function(done) {
-        try{
-          throw new Error("error message ii.");
-        }catch(ex){
-          var ex2 = Sai.error(ex);
-
-          expect(ex2.msg).to.equal("error message ii.");
-          expect(ex2.profile).to.equal("jserror");
-          expect(ex2.hasOwnProperty("file")).to.equal(true);
-          expect(ex2.hasOwnProperty("line")).to.equal(true);
-          expect(ex2.hasOwnProperty("col")).to.equal(true);
-          expect(ex2.hasOwnProperty("num")).to.equal(true);
-          expect(ex2.hasOwnProperty("type")).to.equal(true);
-          expect(ex2.hasOwnProperty("stack")).to.equal(true);
-          expect(ex2.hasOwnProperty("lost")).to.equal(true);
-          expect(ex2.hasOwnProperty("lang")).to.equal(true);
-          expect(ex2.hasOwnProperty("uv")).to.equal(true);
-          expect(ex2.uv).to.equal(1);
-        }
-        done();
-      });
-
-      it("try/catch: Sai.error() repeat, without uv.", function(done) {
-        try{
-          throw new Error("error message ii.");
-        }catch(ex){
-          var ex3 = Sai.error(ex);
-
-          expect(ex3.msg).to.equal("error message ii.");
-          expect(ex3.profile).to.equal("jserror");
-          expect(ex3.hasOwnProperty("file")).to.equal(true);
-          expect(ex3.hasOwnProperty("line")).to.equal(true);
-          expect(ex3.hasOwnProperty("col")).to.equal(true);
-          expect(ex3.hasOwnProperty("num")).to.equal(true);
-          expect(ex3.hasOwnProperty("type")).to.equal(true);
-          expect(ex3.hasOwnProperty("stack")).to.equal(true);
-          expect(ex3.hasOwnProperty("lost")).to.equal(true);
-          expect(ex3.hasOwnProperty("lang")).to.equal(true);
-          // Different error line in phantomjs.
-          //expect(ex3.hasOwnProperty("uv")).to.equal(false);
-        }
-        done();
-      });
-
-
-      after(test_monitor_on);
-
+      Sai.error(new Error("error message."));
     });
-  }
 
+    it("Sai.error() repeat i, without uv.", function(done) {
+      Image.on("fetch", function(src){
+        var url = new Url(src);
+        expect(url.getParam("msg")).to.equal("error message.");
+        expect(url.getParam("profile")).to.equal("jserror");
+        expect(url.getParam("file")).not.to.be(null);
+        expect(url.getParam("line")).not.to.be(null);
+        expect(url.getParam("col")).not.to.be(null);
+        expect(url.getParam("num")).not.to.be(null);
+        expect(url.getParam("type")).not.to.be(null);
+        expect(url.getParam("stack")).not.to.be(null);
+        expect(url.getParam("lost")).not.to.be(null);
+        expect(url.getParam("lang")).not.to.be(null);
+        expect(url.getParam("uv")).to.be(null);
 
-  function test_monitor_on(){
-    describe("Sai.on", function(){
+        Image.off("fetch");
 
-      it("Sai.on('log')", function(done) {
-
-        // NOTE: after for timedSend finished.
-        window.setTimeout(function(){
-          Sai.on("log", function(meta){
-            expect("test").to.equal(meta.seed);
-            done();
-          });
-          Sai.log("test");
-        }, 500);
+        done();
       });
 
-      it("Sai.on('jserror')", function(done) {
-        Sai.on("jserror", function(meta){
-          expect(meta.profile).to.equal("jserror");
-          expect("test error message.").to.equal(meta.msg);
+      // 异常消息必须已抛出过。
+      Sai.error(new Error("error message."));
+    });
+
+    it("try/catch: Sai.error()", function(done) {
+      Image.on("fetch", function(src){
+        var url = new Url(src);
+        expect(url.getParam("msg")).to.equal("error message ii.");
+        expect(url.getParam("profile")).to.equal("jserror");
+        expect(url.getParam("file")).not.to.be(null);
+        expect(url.getParam("line")).not.to.be(null);
+        expect(url.getParam("col")).not.to.be(null);
+        expect(url.getParam("num")).not.to.be(null);
+        expect(url.getParam("type")).not.to.be(null);
+        expect(url.getParam("stack")).not.to.be(null);
+        expect(url.getParam("lost")).not.to.be(null);
+        expect(url.getParam("lang")).not.to.be(null);
+        expect(url.getParam("uv")).to.equal("1");
+
+        Image.off("fetch");
+
+        done();
+      });
+
+      try{
+        throw new Error("error message ii.");
+      }catch(ex){
+        Sai.error(ex);
+      }
+    });
+
+    it("try/catch: Sai.error() repeat ii, without uv.", function(done) {
+      Image.on("fetch", function(src){
+        var url = new Url(src);
+        expect(url.getParam("msg")).to.equal("error message ii.");
+        expect(url.getParam("profile")).to.equal("jserror");
+        expect(url.getParam("file")).not.to.be(null);
+        expect(url.getParam("line")).not.to.be(null);
+        expect(url.getParam("col")).not.to.be(null);
+        expect(url.getParam("num")).not.to.be(null);
+        expect(url.getParam("type")).not.to.be(null);
+        expect(url.getParam("stack")).not.to.be(null);
+        expect(url.getParam("lost")).not.to.be(null);
+        expect(url.getParam("lang")).not.to.be(null);
+        // Different error line in phantomjs.
+        //expect(url.getParam("uv")).to.be(null);
+
+        Image.off("fetch");
+
+        done();
+      });
+
+      try{
+        throw new Error("error message ii.");
+      }catch(ex){
+        Sai.error(ex);
+      }
+    });
+
+
+    after(test_monitor_on);
+
+  });
+}
+
+
+function test_monitor_on(){
+  describe("Sai.on", function(){
+
+    it("Sai.on('log')", function(done) {
+
+      // NOTE: after for timedSend finished.
+      window.setTimeout(function(){
+
+        Image.on("fetch", function(src){
+          var url = new Url(src);
+          expect(url.getParam("seed")).to.equal("test");
+          expect(url.getParam("uid")).to.equal("UID");
+
+          Image.off("fetch");
           done();
         });
 
-        try{
-          throw new Error("test error message.");
-        }catch(ex){
-          Sai.error(ex);
-        }
-      });
+        Sai.on("log", function(meta){
+          expect("test").to.equal(meta.seed);
+          meta.uid = "UID";
+        });
+
+        Sai.log("test");
+
+      }, 500);
     });
-  }
+
+    it("Sai.on('jserror')", function(done) {
+      Image.on("fetch", function(src){
+        var url = new Url(src);
+        expect(url.getParam("profile")).to.equal("jserror");
+        expect(url.getParam("msg")).to.equal("test error message.");
+        expect(url.getParam("PID")).to.equal("PAGE_ID");
+
+        Image.off("fetch");
+        done();
+      });
+      Sai.on("jserror", function(meta){
+        expect(meta.profile).to.equal("jserror");
+        expect("test error message.").to.equal(meta.msg);
+        meta.PID = "PAGE_ID";
+      });
+
+      try{
+        throw new Error("test error message.");
+      }catch(ex){
+        Sai.error(ex);
+      }
+    });
+  });
+}
